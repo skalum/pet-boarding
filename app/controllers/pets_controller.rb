69720be991614d68c57d6ca92ee0a6c52d0b2c26
Sam Kalum <stkalum@gmail.com>
2018-03-15 13:15:17 -0400

    Implement index, show, and new for Pets.

---
+++ app/controllers/pets_controller.rb
@@ -0,0 +1,54 @@
+class PetsController < ApplicationController
+
+  get '/pets' do
+    if logged_in?
+      if current_user.class == Owner
+        erb :'pets/pets'
+      else
+        redirect '/dashboard'
+      end
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/pets/new' do
+    if logged_in?
+      if current_user.class == Owner
+        erb :'pets/new'
+      else
+        redirect '/dashboard'
+      end
+    else
+      redirect '/login'
+    end
+  end
+
+  get 'pets/:id' do
+    if logged_in?
+      @pet = Pet.find_by(id: params[:id])
+      if current_user.pets.include?(@pet)
+        erb :'pets/show'
+      else
+        redirect '/pets'
+      end
+    else
+      redirect '/login'
+    end
+  end
+
+  post '/pets' do
+    if logged_in?
+      pet = Pet.create(params[:pet])
+      if pet
+        pet.update(owner: current_user)
+      else
+        redirect '/pets/new'
+      end
+      redirect '/pets'
+    else
+      redirect '/login'
+    end
+  end
+
+end
--- config.ru
+++ config.ru
@@ -7,4 +7,5 @@ end
 use Rack::MethodOverride
 
 use UsersController
+use PetsController
 run ApplicationController
