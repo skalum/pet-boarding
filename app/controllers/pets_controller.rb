f3da4c540cf23dbbfaced909a6e09ff2ddce14da
Sam Kalum <stkalum@gmail.com>
2018-03-15 13:50:21 -0400

    Implement edit Pets.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -27,8 +27,21 @@ class PetsController < ApplicationController
   get 'pets/:id' do
     if logged_in?
       @pet = Pet.find_by(id: params[:id])
-      if current_user.pets.include?(@pet)
+      if @pet && current_user.class == Owner && current_user.pets.include?(@pet)
         erb :'pets/show'
+      else
+        redirect '/pets'
+      end
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/pets/:id/edit' do
+    if logged_in?
+      @pet = Pet.find_by(id: params[:id])
+      if current_user.class == Owner && current_user.pets.include?(@pet)
+        erb :'pets/edit'
       else
         redirect '/pets'
       end
@@ -40,12 +53,26 @@ class PetsController < ApplicationController
   post '/pets' do
     if logged_in?
       pet = Pet.create(params[:pet])
-      if pet
+      if pet && current_user.class == Owner
         pet.update(owner: current_user)
       else
         redirect '/pets/new'
       end
       redirect '/pets'
+    else
+      redirect '/login'
+    end
+  end
+
+  patch '/pets/:id' do
+    if logged_in?
+      pet = Pet.find_by(id: params[:id])
+      if pet && current_user.class == Owner && current_user.pets.include?(pet)
+        pet.update(params[:pet])
+        redirect "pets/#{pet.id}"
+      else
+        redirect "/pets/#{pet.id}/edit"
+      end
     else
       redirect '/login'
     end
