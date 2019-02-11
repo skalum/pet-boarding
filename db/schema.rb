3519ab85dc2aae381ca3a4c6b64f37ddf289b850
skalum <stkalum@gmail.com>
2019-02-11 16:59:25 -0500

    add timezone to users, user_id to addresses

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180315192712) do
+ActiveRecord::Schema.define(version: 2019_02_11_215421) do
 
   create_table "addresses", force: :cascade do |t|
     t.string "line_1"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20180315192712) do
     t.string "zip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "appointments", force: :cascade do |t|
@@ -54,6 +55,8 @@ ActiveRecord::Schema.define(version: 20180315192712) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
+    t.string "time_zone", default: "UTC"
+    t.string "timezone", default: "America/New_York"
   end
 
 end
